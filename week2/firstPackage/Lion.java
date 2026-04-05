ca154abcdc3a22f9ee1cd34f60ab859150958023
yerin <chayerin123@gmail.com>
2026-04-05 20:18:26 +0900

    week2 - 생성자 초기값 복구

--- week2/firstPackage/Lion.java
+++ week2/firstPackage/Lion.java
@@ -5,10 +5,10 @@ public class Lion {
   String major; // default - 같은 패키지 내에서만 직접 접근 가능
   private int generation; // Lion 클래스 내부에서만 직접 접근 가능
 
-    public Lion() {
-      this.name = "";
-      this.major = "";
-      this.generation = 0;
+    public Lion(String name, String major, int generation) {
+      this.name = name;
+      this.major = major;
+      this.generation = generation;
     }
 
     public int getGeneration() {
